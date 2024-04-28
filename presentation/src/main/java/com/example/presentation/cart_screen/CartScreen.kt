e43f542e5a389303e23bdb165a7a526d69e4e1e5
PozyalovMihail <pozyalov@gmail.com>
2024-04-28 15:20:40 +0300

    добавил CartItem

--- presentation/src/main/java/com/example/presentation/cart_screen/CartScreen.kt
+++ presentation/src/main/java/com/example/presentation/cart_screen/CartScreen.kt
@@ -1,2 +1,47 @@
 package com.example.presentation.cart_screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.presentation.cart_screen.common_item.CartItem
+
+@Composable
+@Preview
+fun CartScreenPreview() {
+    CartScreen(navigateBack = {})
+}
+
+@Composable
+fun CartScreen(
+    navigateBack: () -> Unit
+) {
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+    ) {
+//        DetailTopBar(
+//            homeViewModel = homeViewModel,
+//            isDetailScreen = false,
+//            title = "Detail product",
+//            navigateBack = { navigateBack() }
+//        )
+        LazyColumn(
+            modifier = Modifier.padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            items(5) {
+                CartItem()
+            }
+        }
+    }
+}
---
+++ presentation/src/main/java/com/example/presentation/cart_screen/common_item/CartItem.kt
@@ -0,0 +1,106 @@
+package com.example.presentation.cart_screen.common_item
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Delete
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CheckboxDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.presentation.R
+import com.example.presentation.theme.GrayDarkest
+import com.example.presentation.theme.GrayLight
+import com.example.presentation.theme.GrayLighter
+import com.example.presentation.theme.Mint
+
+@Preview
+@Composable
+fun CartItem() {
+    Row(
+        modifier = Modifier.fillMaxWidth()
+            .background(Color.White)
+            .height(80.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Checkbox(
+            checked = false,
+            onCheckedChange = null,
+            colors = CheckboxDefaults.colors(
+                checkedColor = Mint,
+                uncheckedColor = GrayLighter)
+        )
+        Image(
+            painter = painterResource(id = R.drawable.img),
+            modifier = Modifier
+                .fillMaxHeight()
+                .aspectRatio(1f)
+                .clip(RoundedCornerShape(8.dp)),
+            contentDescription = null,
+            contentScale = ContentScale.Crop
+        )
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.SpaceAround
+        ) {
+            Text(
+                text = "Product name",
+                color = GrayDarkest
+            )
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = "$ 333333",
+                    modifier = Modifier.weight(1f),
+                    color = GrayDarkest
+                )
+                Icon(
+                    painter = painterResource(id = R.drawable.minus),
+                    contentDescription = null,
+                    modifier = Modifier.border(1.dp, GrayLighter, CircleShape),
+                    tint = GrayLight
+                )
+                Text(
+                    text = "1",
+                    color = GrayDarkest
+                )
+                Icon(
+                    painter = painterResource(id = R.drawable.plus),
+                    contentDescription = null,
+                    modifier = Modifier.border(1.dp, GrayLighter, CircleShape),
+                    tint = GrayLight
+                )
+                Icon(
+                    imageVector = Icons.Outlined.Delete,
+                    contentDescription = null,
+                    modifier = Modifier.border(1.dp, GrayLighter, CircleShape),
+                    tint = GrayLight
+                )
+            }
+        }
+    }
+}
