8c760d250583031d5b4e7c9264495a2ab83f7e4e
PozyalovMihail <pozyalov@gmail.com>
2024-05-05 11:05:05 +0300

    fix account screen

--- presentation/src/main/java/com/example/presentation/account_screen/AccountProfileViewModel.kt
+++ presentation/src/main/java/com/example/presentation/account_screen/AccountProfileViewModel.kt
@@ -2,52 +2,46 @@ package com.example.presentation.account_screen
 
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.domain.models.Category
 import com.example.domain.models.User
-import com.example.domain.usecases.user_db_use_case.GetAllUserUseCase
+import com.example.domain.usecases.user_db_use_case.GetIsLoginUserUseCase
 import com.example.domain.usecases.user_db_use_case.SaveUserUseCase
 import com.example.presentation.R
-import com.example.utils.ApiResult
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
-import kotlinx.coroutines.Dispatchers
-
-import kotlinx.coroutines.flow.StateFlow
-
 
 @HiltViewModel
 class AccountProfileViewModel @Inject constructor(
-    private val getAllUsers: GetAllUserUseCase,
-    private val saveUser: SaveUserUseCase
+    private val saveUser: SaveUserUseCase,
+    private val getIsLoginUserUseCase: GetIsLoginUserUseCase
 ) : ViewModel() {
-
-
 
     private val _userState = MutableStateFlow<User?>(null)
     val userState: StateFlow<User?> = _userState
-    val currentProfileImage: MutableState<Int?> = mutableStateOf(userState.value?.image?.toIntOrNull() ?: R.drawable.ic_profile)
-    val _loading: MutableStateFlow<Boolean> = MutableStateFlow(true)
-    val loading = _loading
+
+    val currentProfileImage: MutableState<Int?> = mutableStateOf(userState.value?.image?.toIntOrNull()
+        ?: R.drawable.ic_profile)
 
     init {
-        getUser()
+        loadUserData()
         observeUserState()
     }
 
-    private fun getUser() {
-        viewModelScope.launch(Dispatchers.IO) {
-            _loading.value = true
-            getAllUsers()?.find { it.isLogin }?.let {
-                _userState.value = it
-                _loading.value = false
+    private fun loadUserData() {
+        viewModelScope.launch {
+            while (true) {
+                _userState.value = getIsLoginUserUseCase()
+                delay(1000)
             }
         }
     }
+
     fun signOut() {
         viewModelScope.launch(Dispatchers.IO) {
             _userState.value?.let { currentUser ->
